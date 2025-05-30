206a86e6c7c01f89aec2efe0ba446226cdbbce86
David <asd648515@gmail.com>
2025-05-30 16:43:35 +0800

    Add initial test structure for camera GUI functionality

---
+++ test/model/test_camera_device.py
@@ -0,0 +1,13 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..'))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'app'))
+
+
+def test_camera_gui():
+    pass
+    
+
+
+if __name__ == "__main__":
+    test_camera_gui()

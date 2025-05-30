9fd57c485baa9660f5648df445e3592b02bef552
David <asd648515@gmail.com>
2025-05-30 16:00:51 +0800

    Refactor LEDWindow setup_ui method to remove unused layouts and improve button functionality

--- app/view/led_gui.py
+++ app/view/led_gui.py
@@ -21,10 +21,7 @@ class LEDWindow(QFrame):
         self.setup_ui()
 
     def setup_ui(self):
-        self.main_layout = QVBoxLayout(self)
-        # self.main_layout.setSpacing(10)
 
-        #==== Create first grid layout ====#
         grid1_box = QWidget()
         grid1_box.setStyleSheet("")
         grid1_box.setFixedHeight(40)
@@ -33,9 +30,7 @@ class LEDWindow(QFrame):
         label_LED.setStyleSheet("font-size: 24px; font-weight: bold; color: rgb(0, 0, 0); border: 0px;")
         grid1_layout.addWidget(label_LED, 0, 0)
         grid1_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        #==== Finish ====#
 
-        #===== Create second grid layout =====#
         grid2_box = QWidget()
         grid2_box.setStyleSheet("border: 0px;")
         grid2_box.setFixedHeight(60)
@@ -51,85 +46,56 @@ class LEDWindow(QFrame):
         self.button_func['state_circle'] = state_circle
         grid2_layout.addWidget(state_circle, 0, 1, 1, 3)
         
-        #===== Create third grid layout =====#
         grid3_box = QWidget()
         grid3_box.setStyleSheet("border: 0px solid rgb(0, 0, 0);")
         grid3_layout = QGridLayout(grid3_box)
 
-        self.LED_start_button = button_setup("LED開始運作", lambda: None)
+        self.LED_start_button = button_setup("LED開始運作", lambda: print("LED開始運作"))
         grid3_layout.addWidget(self.LED_start_button, 0, 0, 1, 2)
 
-        self.LED_exit_button = button_setup("LED退出", lambda: None)
+        self.LED_exit_button = button_setup("LED退出", lambda: print("LED退出"))
         grid3_layout.addWidget(self.LED_exit_button, 0, 2, 1, 2)
 
-        self.LED_flicker_3times_button = button_setup("LED閃爍三次", lambda: None)
+        self.LED_flicker_3times_button = button_setup("LED閃爍三次", lambda: print("LED閃爍三次"))
         grid3_layout.addWidget(self.LED_flicker_3times_button, 1, 0, 1, 4)
 
-        self.LED_cycle_flicker_button = button_setup("LED循環閃爍", lambda: None)
+        self.LED_cycle_flicker_button = button_setup("LED循環閃爍", lambda: print("LED循環閃爍"))
         grid3_layout.addWidget(self.LED_cycle_flicker_button, 2, 0, 1, 4)
 
-        self.LED_stop_button = button_setup("LED停止", lambda: None)
+        self.LED_stop_button = button_setup("LED停止", lambda: print("LED停止"))
         grid3_layout.addWidget(self.LED_stop_button, 3, 0, 1, 4)
-        #==== Finish ====#
         
-        #===== Create combine second and third grid layout =====#
         combine_layout2_layout3_box = QWidget()
         combine_layout2_layout3_box.setStyleSheet("background-color: rgb(214, 234, 248); border-radius: 20px; border: 2px solid rgb(0, 0, 0);")
         combine_layout2_layout3 = QVBoxLayout(combine_layout2_layout3_box)
         combine_layout2_layout3.addWidget(grid2_box)
         combine_layout2_layout3.addWidget(grid3_box)
-        #==== Finish ====#
         
-        #==== Create sixth grid layout ====#
-        grid6_box = QWidget()
-        grid6_box.setStyleSheet("border: 0px;")
-        grid6_box.setFixedHeight(40)
-        grid6_layout = QGridLayout(grid6_box)
+        grid4_box = QWidget()
+        grid4_box.setStyleSheet("border: 0px;")
+        grid4_box.setFixedHeight(40)
+        grid4_layout = QGridLayout(grid4_box)
         label_test_record = label_setup("測試紀錄 :", "font-size: 16px; font-family: 微軟正黑體; font-weight: bold;")
-        grid6_layout.addWidget(label_test_record, 0, 0, 1, 4)
-        #==== Finish ====#
+        grid4_layout.addWidget(label_test_record, 0, 0, 1, 4)
 
-        #==== Create seventh grid layout ====#
-        grid7_box = QWidget()
-        grid7_box.setStyleSheet("border: 0px solid rgb(0, 0, 0);")
-        grid7_layout = QGridLayout(grid7_box)
+        grid5_box = QWidget()
+        grid5_box.setStyleSheet("border: 0px solid rgb(0, 0, 0);")
+        grid5_layout = QGridLayout(grid5_box)
         self.entry_test_record = QTextEdit(self)
         self.entry_test_record.setReadOnly(True)
         self.entry_test_record.setStyleSheet("background-color: rgb(255, 255, 255); font-size: 14px; font-family: 微軟正黑體; font-weight: bold; border: 0px; border-radius: 10px; color: rgb(0, 0, 0);")
         self.test_record['record'] = self.entry_test_record
-        grid7_layout.addWidget(self.entry_test_record, 0, 0, 1, 4)
-        #==== Finish ====#
+        grid5_layout.addWidget(self.entry_test_record, 0, 0, 1, 4)
         
-        #==== Create combine sixth and seventh grid layout ====#
         combine_layout6_layout7_box = QWidget()
         combine_layout6_layout7_box.setStyleSheet("background-color: rgb(214, 234, 248); border-radius: 20px; border: 2px solid rgb(0, 0, 0);")
         combine_layout6_layout7 = QVBoxLayout(combine_layout6_layout7_box)
-        combine_layout6_layout7.addWidget(grid6_box)
-        combine_layout6_layout7.addWidget(grid7_box)
-        #==== Finish ====#
+        combine_layout6_layout7.addWidget(grid4_box)
+        combine_layout6_layout7.addWidget(grid5_box)
 
-        #==== Create eighth grid layout ====#
-        grid8_box = QWidget()
-        grid8_box.setStyleSheet("")
-        grid8_layout = QGridLayout(grid8_box)
-
-        button_back = button_setup("上一步", lambda: None)
-        grid8_layout.addWidget(button_back, 0, 0, 1, 1)
-        self.button_func['button_back'] = button_back
-
-        grid8_layout.setSpacing(400)
-
-        button_next = button_setup("下一步", lambda: None)
-        grid8_layout.addWidget(button_next, 0, 1, 1, 1)
-        self.button_func['button_next'] = button_next
         
-        
-
-        #==== Finally, add the grid layout to the main layout ====#
+        self.main_layout = QVBoxLayout(self)
         self.main_layout.addWidget(grid1_box)
         self.main_layout.addWidget(combine_layout2_layout3_box)
-        # self.main_layout.addWidget(grid4_box)
         self.main_layout.addWidget(combine_layout6_layout7_box)
-        self.main_layout.addWidget(grid8_box)
-        #===== Finish =====#
 
